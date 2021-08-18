e17713920b2a0400af1dd2ff22e8070bebeb1459
Nico Ledesma <nicoledesma36@gmail.com>
2021-08-18 16:52:49 -0300

    styles on edit profile

--- frontend/views/user/edit.php
+++ frontend/views/user/edit.php
@@ -12,18 +12,24 @@ $this->registerAssetBundle(UserProfileAsset::class);
 <hr class="border-bottom border-white">
 
 <div class="box-profile">
-    <?php $form = ActiveForm::begin(['id' => 'edit-profile-form']); ?>
+    <?php $form = ActiveForm::begin(['id' => 'edit-profile-form', 'options'=> ['class' => 'd-flex justify-content-center']]); ?>
+    <div class="content-img">
+        <i class="fa fa-user-circle icon-profile" aria-hidden="true"></i>
+    </div>
+    <div class="content-form">
 
-    <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder'=>"Username"]) ?>
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => "Username"]) ?>
 
-    <?= $form->field($model, 'email')->textInput()->input('email', ['placeholder' => "Email"]) ?>
+        <?= $form->field($model, 'email')->textInput()->input('email', ['placeholder' => "Email"]) ?>
 
-    <?= $form->field($model, 'password')->passwordInput(['placeholder' => "Password"]) ?>
+        <?= $form->field($model, 'password')->passwordInput(['placeholder' => "Password"]) ?>
 
-    <?= $form->field($model, 'confirm_password')->passwordInput(['placeholder' => "Confirm Password"]) ?>
+        <?= $form->field($model, 'confirm_password')->passwordInput(['placeholder' => "Confirm Password"]) ?>
 
-    <div class="float-right mt-3">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+
+        <div class="float-right mt-3">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
     </div>
 
 
