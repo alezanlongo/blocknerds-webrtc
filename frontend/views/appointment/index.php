f909d19ff227a7ba49e5d11f15e3d258a5d9728b
Ricardo Cibrián <ricardo.cibrian@plandomino.com>
2021-09-28 00:55:18 -0500

    frontend/views/appointment: Fix title text

--- frontend/views/appointment/index.php
+++ frontend/views/appointment/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Patients';
+$this->title = 'Appointments';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="patient-index">
