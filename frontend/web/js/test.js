890066b184d5e9a17c6f18a370c06300744794e7
NB <nbulian@nonox-one.fibertel.com.ar>
2021-09-30 18:06:51 -0300

    Improving view

--- frontend/web/js/test.js
+++ frontend/web/js/test.js
@@ -1,4 +1,4 @@
-const TIME_OUT_MILLISECONDS = 3000;
+const TIME_OUT_MILLISECONDS = 5000;
 const KIND_AUDIO_INPUT = "audioinput";
 const KIND_VIDEO_INPUT = "videoinput";
 const KIND_AUDIO_OUTPUT = "audiooutput";
@@ -15,12 +15,15 @@ const {
 } = Twilio.Diagnostics;
 
 console.log("twilio data to diagnostics", Twilio.Diagnostics, Twilio);
+
 const getDevicesConnected = () => {
   return navigator.mediaDevices.enumerateDevices();
 };
+
 const filterDevices = (devices, kind) => {
   return devices.filter((device) => device.kind === kind);
 };
+
 getDevicesConnected()
   .then((devices) => {
     console.log("devices", devices);
@@ -37,72 +40,127 @@ navigator.mediaDevices
     initTests();
   })
   .catch(function (err) {
-    console.log("err", err);
+    console.log("mediaDevices error", err);
+    $("#testAudioVideoDevices h1").append('<i class="text-danger fas fa-exclamation-triangle"></i>');
+    $("#testAudioVideoDevices").append('<p class="text-danger">Failed to access your computer\'s camera and microphone</p>');
+    $("#testAudioVideoDevices").append('<p class="text-danger">' + err + '</p>');
   });
 
-const initTests = (devices) => {
+const initTests = () => {
 
   doAudioOutputTest();
 
   doAudioInputTest();
 
   doVideoInputTest($('#video-test'));
-};
 
-const doAudioInputTest = (deviceId = 'default') => {
-  const audioInputDeviceTest = testAudioInputDevice({
-    deviceId,
-  });
-  console.log("audioInputDeviceTest", audioInputDeviceTest);
-
-  audioInputDeviceTest.on(EVENT_VOLUME, (volume) => {
-    console.log("volume", volume);
-  });
-
-  audioInputDeviceTest.on(EVENT_ERROR, (error) => {
-    console.error("error", error);
-  });
-
-  audioInputDeviceTest.on(EVENT_END, (report) => {
-    console.log("report", report);
-  });
-
-  stopTest(audioInputDeviceTest);
+  doBitrateTest();
 };
 
 const doAudioOutputTest = (deviceId = 'default') => {
+
+  $("#testAudioOutputDevice").removeClass("text-secondary");
+
   const audioOutputDeviceTest = testAudioOutputDevice({
     deviceId,
   });
 
   audioOutputDeviceTest.on(EVENT_VOLUME, (volume) => {
-    console.log("VOLUME", volume);
+    console.log("audioOutputDeviceTest volume", volume);
   });
 
   audioOutputDeviceTest.on(EVENT_ERROR, (error) => {
-    console.error("ERROR", error);
+    console.error("audioOutputDeviceTest error", error);
+    $("#testAudioOutputDevice").addClass("text-danger");
   });
 
   audioOutputDeviceTest.on(EVENT_END, (report) => {
-    console.log("report", report);
+    console.log("audioOutputDeviceTest report", report);
+
+    if (report.errors.length <= 0) {
+      $("#testAudioOutputDevice").addClass("text-success");
+      $("#testAudioOutputDevice h4").append('<i class="fas fa-check"></i>');
+    } else {
+      $("#testAudioOutputDevice h4").append('<i class="fas exclamation-triangle"></i>');
+      report.errors.forEach(err => {
+        $("#testAudioOutputDevice").append('<p class="text-danger">' + err.message + '</p>');
+      });
+    }
+
   });
+
   stopTest(audioOutputDeviceTest);
 };
 
+const doAudioInputTest = (deviceId = 'default') => {
+
+  $("#testAudioInputDevice").removeClass("text-secondary");
+
+  const audioInputDeviceTest = testAudioInputDevice({
+    deviceId,
+  });
+
+  console.log("audioInputDeviceTest", audioInputDeviceTest);
+
+  audioInputDeviceTest.on(EVENT_VOLUME, (volume) => {
+    console.log("audioInputDeviceTest volume", volume);
+  });
+
+  audioInputDeviceTest.on(EVENT_ERROR, (error) => {
+    console.error("audioInputDeviceTest error", error);
+    $("#testAudioInputDevice").addClass("text-danger");
+  });
+
+  audioInputDeviceTest.on(EVENT_END, (report) => {
+    console.log("audioInputDeviceTest report", report);
+
+    if (report.errors.length <= 0) {
+      $("#testAudioInputDevice").addClass("text-success");
+      $("#testAudioInputDevice h4").append('<i class="fas fa-check"></i>');
+    } else {
+      $("#testAudioInputDevice h4").append('<i class="fas exclamation-triangle"></i>');
+      report.errors.forEach(err => {
+        $("#testAudioInputDevice").append('<p class="text-danger">' + err.message + '</p>');
+      });
+    }
+
+  });
+
+  stopTest(audioInputDeviceTest);
+};
+
 const doVideoInputTest = (element) => {
+
+  $("#testVideoInputDevice").removeClass("text-secondary");
+
   const videoInputDeviceTest = testVideoInputDevice({ element: $(element).get(0) });
+
   videoInputDeviceTest.on(EVENT_ERROR, (error) => {
-    console.error(error);
+    console.error("videoInputDeviceTest error", error);
+    $("#testVideoInputDevice").addClass("text-danger");
   });
 
   videoInputDeviceTest.on(EVENT_END, (report) => {
-    console.log("report", report);
+    console.log("videoInputDeviceTest report", report);
+
+    if (report.errors.length <= 0) {
+      $("#testVideoInputDevice").addClass("text-success");
+      $("#testVideoInputDevice h4").append('<i class="fas fa-check"></i>');
+    } else {
+      $("#testVideoInputDevice h4").append('<i class="fas exclamation-triangle"></i>');
+      report.errors.forEach(err => {
+        $("#testVideoInputDevice").append('<p class="text-danger">' + err.message + '</p>');
+      });
+    }
   });
 
   stopTest(videoInputDeviceTest);
 };
 
 const doBitrateTest = () => {
+
+  $("#testMediaConnectionBitrate").removeClass("text-secondary");
+
   const mediaConnectionBitrateTest = testMediaConnectionBitrate({
     iceServers: [
       {
@@ -114,15 +172,26 @@ const doBitrateTest = () => {
   });
 
   mediaConnectionBitrateTest.on("bitrate", (bitrate) => {
-    console.log(bitrate);
+    console.log("mediaConnectionBitrateTest bitrate", bitrate);
   });
 
   mediaConnectionBitrateTest.on("error", (error) => {
-    console.log(error);
+    console.log("mediaConnectionBitrateTest error", error);
+    $("#testMediaConnectionBitrate").addClass("text-danger");
   });
 
   mediaConnectionBitrateTest.on("end", (report) => {
-    console.log(report);
+    console.log("mediaConnectionBitrateTest report", report);
+
+    if (report.errors.length <= 0) {
+      $("#testMediaConnectionBitrate").addClass("text-success");
+      $("#testMediaConnectionBitrate h4").append('<i class="fas fa-check"></i>');
+    } else {
+      $("#testMediaConnectionBitrate h4").append('<i class="fas exclamation-triangle"></i>');
+      report.errors.forEach(err => {
+        $("#testMediaConnectionBitrate").append('<p class="text-danger">' + err.message + '</p>');
+      });
+    }
   });
 
   stopTest(mediaConnectionBitrateTest);
