6d84aff6dd06a91b89f53bd1ad256b1556dea20a
NichollsLedesma <nicoledesma36@gmail.com>
2021-09-30 12:40:30 -0300

    remove files lib folder

--- frontend/web/js/test.js
+++ frontend/web/js/test.js
@@ -25,7 +25,7 @@ getDevicesConnected()
     console.log("devices", devices);
     const optionsDevices = filterDevices(devices, KIND_AUDIO_INPUT)
     console.log('options',optionsDevices)
-    // deviceSelected = optionsDevices[0];
+    deviceSelected = optionsDevices[0];
     initTests();
   })
   .catch((err) => console.log(err));
@@ -40,10 +40,10 @@ getDevicesConnected()
 //     console.log("err", err);
 //   });
 const initTests = (devices) => {
-//   const outputHeadphonesDeviceId = "1f7677ca74b7d45fa99f19f46187e71afc72454d3bee888e8371a74b6a2b9ee0";
-//   doAudioOutputTest(outputHeadphonesDeviceId)
-//     const micHeadphonesDeviceId = "92c35b5b3166c069b8c0f31d2a3a753c420a312982ccb425d92df1b765241ceb"
-//   doAudioInputTest(micHeadphonesDeviceId);
+  const outputHeadphonesDeviceId = "1f7677ca74b7d45fa99f19f46187e71afc72454d3bee888e8371a74b6a2b9ee0";
+  doAudioOutputTest(outputHeadphonesDeviceId)
+    const micHeadphonesDeviceId = "92c35b5b3166c069b8c0f31d2a3a753c420a312982ccb425d92df1b765241ceb"
+  doAudioInputTest(micHeadphonesDeviceId);
   
 };
 
