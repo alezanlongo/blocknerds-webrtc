6aa2af84b4e4c185830d649cd75dd540c5bf8edc
NichollsLedesma <nicoledesma36@gmail.com>
2021-10-06 12:11:23 -0300

    fixes modals styles

--- common/widgets/cardNextOrInProgressMeeting/views/_modal.php
+++ common/widgets/cardNextOrInProgressMeeting/views/_modal.php
@@ -9,12 +9,12 @@ $formatter = \Yii::$app->formatter;
 <div class="row m-1">
     <div class="col-sm-4">
         <div class="card">
-            <div class="card-body d-flex ">
-                <div class="flex-grow-1">
+            <div class="card-body d-flex row">
+                <div class="flex-grow-1 col">
                     <h5 class="card-title "><?= $title ?></h5>
                     <p class="card-text "><?= $text ?></p>
                 </div>
-               <div>
+               <div class="col">
                <?php
                 echo Html::a(
                     "Go to meeting",
