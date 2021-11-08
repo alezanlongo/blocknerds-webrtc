0b70e2ab4464a494bb1dfa7942eea46dbe622553
Eduardo Salas <ezsb82@gmail.com>
2021-11-08 15:35:51 -0300

    Problem view

---
+++ frontend/controllers/ProblemController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\AccessControl;
+use yii\data\ActiveDataProvider;
+use common\components\AthenaComponent;
+use common\components\Athena\models\Problem;
+
+class ProblemController extends Controller
+{
+    private $component;
+
+    public function init()
+    {
+        parent::init();
+        if($user = Yii::$app->user->identity)
+        {
+            $practiceId = $user->ext_practice_id;
+            $this->component = Yii::createObject(AthenaComponent::class);
+            $this->component->setPracticeid($practiceId);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                "class" => AccessControl::class,
+                "rules" => [
+                    [
+                        'allow' => true,
+                        'roles' => ["@"],
+                    ]
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Problem::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+
+    /**
+     * Displays a single Problem model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Finds the Problem model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Problem the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Problem::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
