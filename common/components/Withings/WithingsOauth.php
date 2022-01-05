160929cfac1a92d0b87fcfde6af783daeccea4ab
NichollsLedesma <nicoledesma36@gmail.com>
2022-01-05 15:08:50 -0300

    several stuff

---
+++ common/components/Withings/WithingsOauth.php
@@ -0,0 +1,160 @@
+<?php
+namespace common\components\Athena;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\db\Exception as DBException;
+use yii\httpclient\Client;
+use yii\httpclient\Exception as HttpClientException;
+
+use common\models\Configuration;
+
+class WithingsOauth
+{
+    const URL_SERVICE_AUTH = "oauth2/v1/token";
+
+    /**
+     * AthenaOauth constructor.
+     */
+    public function Authenticate($expired = FALSE)
+    {
+        //REFACTORIZAR ESTE VALOR DEL PRACTICE ID
+        $data = [];
+        $success =  FALSE;
+
+        $configurationModel = Configuration::find()->where([
+            'type'          => 'AthenaToken',
+            'practiceId' => Yii::$app->params['practiceID'],
+        ])->one();
+        if(!is_null($configurationModel)){
+            if($expired){
+                $configurationModel->content = json_encode($this->callAuthenticateAthena());
+                $configurationModel->save();
+            }
+        }else{
+            $configurationModel = new Configuration;
+            $configurationModel->type = "AthenaToken";
+            $configurationModel->content = json_encode($this->callAuthenticateAthena());
+            $configurationModel->practiceId = Yii::$app->params['practiceID'];
+            $configurationModel->save();
+        }
+
+        return $configurationModel->content;
+    }
+
+
+    public function callMethod($path, $action, array $params = [])
+    {
+        $data = [];
+        $success =  FALSE;
+
+        $dataSession = json_decode($this->Authenticate(), TRUE);
+        if((int)$dataSession['expirationTime'] < (int)time()){
+            $dataSession = json_decode($this->Authenticate(TRUE), TRUE);
+        }
+
+        $link = Yii::$app->params['athena_url'].$path;
+        $client = new Client([
+            'transport' => 'yii\httpclient\CurlTransport'
+        ]);
+        $request = $client->createRequest();
+        $requestOptions = [];
+        if(!empty(Yii::$app->params['http_client_timeout'])){
+            $requestOptions[CURLOPT_TIMEOUT] = Yii::$app->params['http_client_timeout']; // set timeout to 5 seconds for the case server is not responding
+        }
+        if(!empty($requestOptions)){
+            $request->setOptions($requestOptions);
+        }
+        $request->setMethod($action)
+            ->setUrl($link)
+            ->setHeaders([
+                'Authorization' => 'Bearer ' . $dataSession['access_token'],
+            ]);
+        if(count($params) > 0){
+            $request->setData($params);
+        }
+
+        try {
+            $response = $request->send();
+        }catch(\Exception $e){
+            throw new \yii\web\ServerErrorHttpException($this->handleError($e->getMessage()));
+        }
+
+        $dataResponse = json_decode($response->getContent(), TRUE);
+        $dataStatusCodeRespose =  $response->getStatusCode();
+        if ($response->isOk) {
+            $success = TRUE;
+            $data = [
+                'message'       => "",
+                'data'          => $dataResponse,
+                'success'       => $success,
+                'statusCode'    => $dataStatusCodeRespose
+            ];
+        }else{
+            $error = '';
+            if(isset($dataResponse['error'])){
+                $error = $dataResponse['error'];
+                $error = (isset($dataResponse['detailedmessage'])) ? $error." ".$dataResponse['detailedmessage'] : $error;
+            }
+            throw new \yii\web\BadRequestHttpException($error);
+        }
+
+
+        return $data;
+    }
+
+
+    private function callAuthenticateAthena()
+    {
+        $link = Yii::$app->params['athena_url'].self::URL_SERVICE_AUTH;
+        $client = new Client();
+        $request = $client->createRequest();
+        $request->setMethod('POST')
+            ->setUrl($link)
+            ->setHeaders([
+                'Content-type' => 'application/x-www-form-urlencoded',
+                'Authorization' => 'Basic ' . base64_encode(Yii::$app->params['athena_key'] . ':' . Yii::$app->params['athena_secret']),
+            ])
+            ->setData([
+                'grant_type'    => 'client_credentials',
+                'scope'         => 'athena/service/Athenanet.MDP.*',
+            ]);
+
+        try {
+            $response = $request->send();
+        } catch (\Exception $e) {
+            throw new \yii\web\ServerErrorHttpException($e->getMessage());
+        }
+
+        $dataResponse = json_decode($response->getContent(), TRUE);
+        $dataStatusCodeRespose =  $response->getStatusCode();
+        if ($response->isOk) {
+            $success = TRUE;
+            $dataResponse['expirationTime'] = (time() + $dataResponse['expires_in']) - 60;
+        }else{
+            $error = '';
+            if(isset($dataResponse['error'])){
+                $error = $dataResponse['error'];
+                $error = (isset($dataResponse['detailedmessage'])) ? $error." ".$dataResponse['detailedmessage'] : $error;
+            }
+            throw new \yii\web\BadRequestHttpException($error);
+        }
+
+        return $dataResponse;
+    }
+
+    private function handleError($errorMessage)
+    {
+        $transportError = 'Curl error: #';//yiisoft\yii2-httpclient\src\CurlTransport.php
+        switch (true){
+            case stristr($errorMessage,$transportError.CURLE_OPERATION_TIMEDOUT):
+                $humanized = 'Communication Error';
+               break;
+            default:
+                $humanized = 'Internal Error';
+         }
+
+         return $humanized;
+    }
+}
--- frontend/controllers/WithingController.php
+++ frontend/controllers/WithingController.php
@@ -9,6 +9,10 @@ use yii\httpclient\Client;
 
 class WithingController extends \yii\web\Controller
 {
+    public $baseUrl = "https://account.withings.com/";
+    public $clientId;
+    public $customerSecret;
+
     const RESPONSE_TYPE_CODE = "code";
     const REQUEST_TYPE_ACTION = "requesttoken";
     const SCOPE_USER_ACTIVITY = "user.activity";
@@ -16,61 +20,156 @@ class WithingController extends \yii\web\Controller
     const SCOPE_USER_INFO = "user.info";
     const GRANT_TYPE_AUTH_CODE = "authorization_code";
 
+    public function init()
+    {
+        parent::init();
+        $this->clientId = Yii::$app->params['withings.clientId'];
+        $this->customerSecret = Yii::$app->params['withings.consumerSecret'];
+    }
+
     public function actionIndex()
     {
-        $basePath = "https://account.withings.com/";
         $source = "oauth2_user/authorize2";
-        $clientId = Yii::$app->params['withings.clientId'];
-        
+        $redirect_uri = Yii::$app->params['withings.redirect_uri'];
+
         // $client = new Client(['baseUrl' => $basePath]);
         $state = "something";
-        $scopes = self::SCOPE_USER_INFO . ',' . self::SCOPE_USER_METRICS .','.self::SCOPE_USER_ACTIVITY;
+        $scopes = self::SCOPE_USER_INFO . ',' . self::SCOPE_USER_METRICS . ',' . self::SCOPE_USER_ACTIVITY;
 
         $params = [
             'response_type' => self::RESPONSE_TYPE_CODE,
-            'client_id' => $clientId,
+            'client_id' => $this->clientId,
             'state' => $state,
             'scope' => $scopes,
-            // 'redirect_uri' => "https://localhost/withing/callback",
-            'redirect_uri' => "https://www.google.com/?hl=es",
+            'redirect_uri' => $redirect_uri,
         ];
-        $url = $basePath . $source . '?' . urldecode(http_build_query($params));
+        $url = $this->baseUrl . $source . '?' . urldecode(http_build_query($params));
         return $this->redirect($url);
     }
     public function actionCallback(string $code, string $state)
     {
-        $basePath = "https://wbsapi.withings.net/";
         $source = "v2/oauth2";
-        $clientId = Yii::$app->params['withings.clientId'];
-        $customerSecret = Yii::$app->params['withings.consumerSecret'];
         $nonce = "nonce";
         $signature = "";
         $params = [
             'action' => self::REQUEST_TYPE_ACTION,
-            'client_id' => $clientId,
+            'client_id' => $this->clientId,
             // 'nonce' => $nonce,
             // 'signature' => $signature,
-            'client_secret' => $customerSecret,
-            'grant_type' => self::GRANT_TYPE_AUTH_CODE ,
+            'client_secret' => $this->customerSecret,
+            'grant_type' => self::GRANT_TYPE_AUTH_CODE,
             'code' => $code,
             'redirect_uri' => "https://www.google.com/?hl=es",
         ];
-        VarDumper::dump( $params, $depth = 10, $highlight = true);
+        VarDumper::dump($params, $depth = 10, $highlight = true);
         die;
-        $client = new Client(['baseUrl' => $basePath]);
+        $client = new Client(['baseUrl' => $this->baseUrl]);
         $response = $client->post($source, $params)->send();
         if ($response->isOk) {
-            VarDumper::dump( ['OK',$response->data], $depth = 10, $highlight = true);
+            VarDumper::dump(['OK', $response->data], $depth = 10, $highlight = true);
             die;
         }
-        VarDumper::dump( ['KO',$response], $depth = 10, $highlight = true);
+        VarDumper::dump(['KO', $response], $depth = 10, $highlight = true);
         die;
+    }
+
+    // public function actionGetDataHashed()
+    // {
+    //     $nonce         = $this->requestNonce();
+    //     VarDumper::dump($nonce, $depth = 10, $highlight = true);
+    //     die;
+    //     $signed_params = array(
+    //         'action'     => 'activate',
+    //         'client_id'  => $this->clientId,
+    //         'nonce'      => $nonce,
+    //     );
+    //     ksort($signed_params);
+    //     $data = implode(",", $signed_params);
+    //     $signature = hash_hmac('sha256', $data, $this->customerSecret);
+    //     Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+    //     return [
+    //         'signature' => $signature,
+    //         'nonce' => $nonce,
+    //     ];
+    // }
+
+    public function actionGetDataHashed()
+    {
+        $nonce = $this->getNonce();
+        //Yii::$app->security->generateRandomString(12);
+
+    }
+
+    private function buildSignature(string $action, int $time)
+    {
+        $signed_params = array(
+            'action'     => $action,
+            'client_id'  => $this->clientId,
+            'timestamp'      => $time,
+        );
+        ksort($signed_params);
+        $data = implode(",", $signed_params);
+
+        return hash_hmac('sha256', $data, $this->customerSecret);
+    }
+
+    // public function actionGetSignature($action)
+    // {
+    //     $signature = $this->buildSignature($action);
+    //     Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+    //     return ['signature' => $signature];
+    // }
+
+    public function actionGetNonce()
+    {
+        $action = 'getnonce';
+        $time = time();
+        $signature = $this->buildSignature($action, $time);
+        $params = [
+            'action' => $action,
+            'client_id' => $this->clientId,
+            'timestamp' => $time,
+            'signature' => $signature,
+        ];
+        $response  = $this->doRequest('v2/signature', $params);
+
+        VarDumper::dump($response, $depth = 10, $highlight = true);
+        die;
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+        return [
+            'data' => $response,
+
+        ];
+    }
+
+    private function doRequest(string $resource, array $params = [], string $method = 'POST')
+    {
+        $client = new Client(['baseUrl' => $this->baseUrl]);
+        $response = $client->post($resource, $params)->send();
+        if ($response->isOk) {
+
+            VarDumper::dump("seems good", $depth = 10, $highlight = true);
+            VarDumper::dump($response, $depth = 10, $highlight = true);
+            die;
+            return $response->data;
+        }
+
+
+        // VarDumper::dump("bad request", $depth = 10, $highlight = true);
+
+        // VarDumper::dump($client, $depth = 10, $highlight = true);
+        // VarDumper::dump($response, $depth = 10, $highlight = true);
+        // die;
+        return null;
     }
 }
 
 // GET SIGNATURE
- // $client_secret =  Yii::$app->params['withings.clientId'];
-        // $client_id     = Yii::$app->params['withings.consumerSecret'];
+ // $client_secret =  Yii::$app->params['withings.consumerSecret'];
+        // $client_id     = Yii::$app->params['withings.clientId'];
         // $nonce         = 'The nonce I retrieved using service: Signature v2 - Getnonce';
         
         // $signed_params = array(
